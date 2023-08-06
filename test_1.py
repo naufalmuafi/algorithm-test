2415063f19bb345ce54f6951ce46188128b590ad
Naufal Muafi <nmuafi1@gmail.com>
2023-08-07 01:57:18 +0700

    add: create a timeout to generate value function

--- test_1.py
+++ test_1.py
@@ -1,19 +1,28 @@
 import random
 import time
 
-def generate_value(V):
+def generate_value(V_old):
   V_new = random.randint(1)
   
-  while abs(V - V_new) > 10:
+  # timeout 0.5 second to the program
+  time0 = time.time()
+  
+  while abs(V_old - V_new) > 10:
     V_new = random.randint(1)
+    
+    if(time.time() - time0 == 0.5):
+      V_new = V_old + 10
     
   return V_new
 
 def main():
-  
+  A = []
+  V, total = 0, 0
   
   while True:
-    
+    V = generate_value(V)
+    A.append(V)    
+    print(f"Water Volume = {V} m^3/s\n")
     
     time.sleep(1)
 

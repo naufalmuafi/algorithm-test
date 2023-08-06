ca0d79c297ea532ce3cd5342386daa81269ac2eb
Naufal Muafi <nmuafi1@gmail.com>
2023-08-07 02:26:37 +0700

    fix: it's working. speed up the timeout, and print the instruction

--- test_1.py
+++ test_1.py
@@ -8,34 +8,46 @@ def generate_value(V_old, cond):
   
   V_new = random.randint(1, 1000)
   
-  # timeout 0.5 second to the program
+  # timeout 0.1 second to the program
   time0 = time.time()
   
   while abs(V_old - V_new) > 10:
     V_new = random.randint(1, 1000)
     
-    if(time.time() - time0 == 0.5):
+    if(time.time() - time0 == 0.1):
       V_new = V_old + 10
     
   return V_new
 
 def main():
+  instruction = """
+  
+  ==============================================
+  Welcome to Calculate Mean Program
+  to stop the program, please enter Ctrl+C
+  
+  Naufal Mu'afi - nmuafi1@gmail.com
+  ==============================================
+  
+  """
+  print(instruction)
+  time.sleep(5)
+  
   A = []
   V, total, count, condition = 0, 0, 1, 1
   
   while True:
     V = generate_value(V, condition)
-    print(f"Water Volume = {V} m^3/s\n")
+    print(f"Water Volume = {V} m^3/s")
     
     total += V
     A.append(V)
     length = len(A)
     
     M = total/length    
-    print(f"The average water volume at second {count} is {M} m^3/s")
+    print(f"The average water volume at second {count} is {M:.3f} m^3/s\n")
     
     count += 1
     time.sleep(1)
 
-if __name__ == "__main":
-  main()
+main()

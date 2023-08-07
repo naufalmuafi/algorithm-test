e3d2f80a6cb17586fbdf499c1bd3f632b64b4c04
Naufal Muafi <nmuafi1@gmail.com>
2023-08-07 16:25:42 +0700

    add: it's working.

--- test_2.py
+++ test_2.py
@@ -6,19 +6,21 @@ def generate_value(V_old, cond):
   if cond == 1:
     V_new = random.randint(1, 10)
     cond = 0
-
-  V_new = random.randint(1, 1000)
-
-  # timeout 0.1 second to the program
-  time0 = time.time()
-
-  while abs(V_old - V_new) > 10:
+    
+    return V_new, cond
+  else:
     V_new = random.randint(1, 1000)
 
-    if(time.time() - time0 == 0.1):
-      V_new = V_old + 10
+    # timeout 0.1 second to the program
+    time0 = time.time()
 
-  return V_new
+    while abs(V_old - V_new) > 10:
+      V_new = random.randint(1, 1000)
+
+      if(time.time() - time0 == 0.1):
+        V_new = V_old + 10
+
+    return V_new, cond
 
 def insertion_sort(data, value):
   for i, v in enumerate(data):
@@ -29,17 +31,28 @@ def insertion_sort(data, value):
   sorted_data = data + [value]
   return sorted_data
 
-def median(data):
-  length = len(data)
-  middle = (length - 1) // 2
+def print_data(data):
+  n = len(data)
+  mid = n // 2
+  
+  if n <= 20:
+    print(f"A = {data} len = {n}")
+  else:
+    if n % 2 == 1:
+      print(f"A = [..., {data[mid-1]}, {data[mid]}, {data[mid+1]}, ...] len = {n}")
+    else:
+      print(f"A = [..., {data[mid-2]}, {data[mid-1]}, {data[mid]}, {data[mid+1]}, ...] len = {n}")
 
-  if middle % 2:
+def median(data):
+  length = len(data)  
+  middle = length // 2
+  
+  if length % 2 == 1:
     Median = data[middle]
   else:
-    middle_even = (data[middle] + data[middle + 1]) / 2.0
-    Median = data[middle_even]
+    Median = (data[middle] + data[middle - 1]) / 2.0
   
-  return length, Median
+  return Median
 
 def main():
   instruction = """
@@ -55,20 +68,17 @@ def main():
   print(instruction)
   time.sleep(5)
 
-  A = []
+  A = [0]
   V, count, condition = 0, 1, 1
 
   while True:
-    V = generate_value(V, condition)    
+    V, condition = generate_value(V, condition)    
     print(f"Water Volume = {V} m^3/s")        
     
-    A = insertion_sort(A, V)
+    A = insertion_sort(A, V)            
+    M = median(A)    
     
-    n, M = median(A)
-    
-    if n <= 15:
-      print(f"A = {A}")
-
+    print_data(A)
     print(f"The median of water volume at second {count} is {M:.2f} m^3/s\n")
 
     count += 1
